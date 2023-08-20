2e3c9e788d318882736cc3c36f5091b6aaf932ce
LuksTapia <lucas.tapia@hotmail.com>
2023-08-20 17:20:26 -0300

    Changing methods

--- src/main/java/org/example/testing/calculators/NumberToRoman.java
+++ src/main/java/org/example/testing/calculators/NumberToRoman.java
@@ -1,24 +1,35 @@
 package org.example.testing.calculators;
 
 import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.Map;
 
         public class NumberToRoman {
 
-        private int[] values = {1000,900,500,400,100,90,50,40,10,9,5,4,1};
-        private String[] romanLetters = {"M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"};
+
 
         public String intToRoman(int num)
         {
             System.out.println("Integer: " + num);
-            HashMap<Integer,String> letters = new HashMap<>();
-            for (int i = 0; i < values.length; i++) {
-                letters.put(values[i],romanLetters[i]);
-            }
+
+            HashMap<Integer, String> romanMap = new LinkedHashMap<>();
+            romanMap.put(1000, "M");
+            romanMap.put(900, "CM");
+            romanMap.put(500, "D");
+            romanMap.put(400, "CD");
+            romanMap.put(100, "C");
+            romanMap.put(90, "XC");
+            romanMap.put(50, "L");
+            romanMap.put(40, "XL");
+            romanMap.put(10, "X");
+            romanMap.put(9, "IX");
+            romanMap.put(5, "V");
+            romanMap.put(4, "IV");
+            romanMap.put(1, "I");
 
             String roman = new String();
 
-            for (Map.Entry<Integer,String> element : letters.entrySet()) {
+            for (Map.Entry<Integer,String> element : romanMap.entrySet()) {
 
                 while(num >= element.getKey()) {
                     roman += element.getValue();
